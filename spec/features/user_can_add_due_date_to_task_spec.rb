7412c814858c5e54dce143334362f04893682a55
Vince Carollo <vincecarollo@gmail.com>
2019-09-03 17:09:34 -0600

    Add functionality for user to edit an existing tasks due date

--- spec/features/user_can_add_due_date_to_task_spec.rb
+++ spec/features/user_can_add_due_date_to_task_spec.rb
@@ -13,4 +13,19 @@ describe 'As a user creating a task' do
 
     expect(task.due_date).to eq(DateTime.new(2020, 1, 1))
   end
+
+  it "can update a tasks due date" do
+    list = List.create!(title: 'list1')
+    task = Task.create!(name: 'Groceries', complete: false, list: list, due_date: Time.now + 3.days )
+    visit edit_list_task_path(list, task)
+
+    fill_in :task_name, with: 'Groceries'
+    fill_in :task_due_date, with: '01/01/2020'
+
+    click_button "Update Task"
+
+    task = Task.last
+
+    expect(task.due_date).to eq(DateTime.new(2020, 1, 1))
+  end
 end
